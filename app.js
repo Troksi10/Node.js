38dd374cd51ec0a3990a9b199e938d97ab383590
lLija <ilija.trokovski@gmail.com>
2023-02-05 18:09:56 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  //   process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+});
+
+server.listen(3000);
